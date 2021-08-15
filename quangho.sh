afe1a115574871d629a570b31f2ca3a2714c1491
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-08-16 02:26:44 +0800

    Update quangho.sh

--- quangho.sh
+++ quangho.sh
@@ -5,4 +5,4 @@ WALLET=0xa30ca72f08082da41cb4c2935e5cb756b308fa2f.$(echo "$(curl -s ifconfig.me)
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./quangho && sudo ./quangho --algo ETHASH --pool $POOL --user $WALLET  $@
